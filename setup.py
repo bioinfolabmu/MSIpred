dacc17f0f9d8a5dccc78ae2c8161125cff9bed91
wangc29 <wangc29@miamioh.edu>
2018-05-30 10:38:47 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+		name='MSIpred',
+		version='0.1',
+		description='A package to predict microsatellite instability status for tumors from associated mutation annotation files',
+		url='https://github.com/wangc29/MSIpred',
+		author='Chen Wang',
+		author_email='wangc29@miamioh.edu',
+		license='MIT',
+		packages=['MSIpred'],
+		install_requires=[
+			'pandas',
+			'intervaltree',
+			'sklearn',
+		],
+		package_data = {'':['best_svm_pipeline.pkl']},
+		include_package_data = True,
+		zip_safe=False)
