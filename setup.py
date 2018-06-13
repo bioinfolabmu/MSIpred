9e1bf1581940946d583476649ec5d4fd34725b9f
wangc29 <wangc29@miamioh.edu>
2018-06-13 08:13:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
 		name='MSIpred',
 		version='0.1',
 		description='A package to predict microsatellite instability status for tumors from associated mutation annotation files',
-		url='https://github.com/wangc29/MSIpred',
+		url='https://github.com/bioinfolabmu/MSIpred',
 		author='Chen Wang',
 		author_email='wangc29@miamioh.edu',
 		license='MIT',
